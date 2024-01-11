38ab7804c8ee889797197dd2107ea9069c0e064d
Victor Volkov <vityan2504@gmail.com>
2024-01-11 11:54:16 +0300

    Перенёс первую страницу на vue

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,18 @@
 <template>
-  <div id="app">
-   hello world!
+  <div class="wrapper">
+    <hero-view />
+    <footer-component />
   </div>
 </template>
 
 <script>
+import HeroView from "@/views/HeroView.vue";
+import FooterComponent from "@/components/FooterComponent.vue";
 
 export default {
-
-}
+  components: { HeroView, FooterComponent },
+};
 </script>
 
 <style>
-
 </style>
